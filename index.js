2eeacc8e528ae485c11e0db6bbd237703d7ef1fc
Dmitry Sadovsky <dimailn9@gmail.com>
2023-03-29 12:15:55 +0300

    fix for new version

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 const coffee = require('coffeescript')
 const babelJest = require('babel-jest')
-const babelPresentEnv = require('@babel/preset-env')
-const babelTransformRuntime = require('@babel/plugin-transform-runtime')
 
 module.exports = {
-  process: (src, path) => {
+  process: (src, path, options) => {
     if (coffee.helpers.isCoffee(path)) {
-       const result = coffee.compile(src, {
+       const js = coffee.compile(src, {
         bare: true,
-        sourceMap: true,
-        filename: path,
-        transpile: {presets: [babelPresentEnv],
-        plugins:  [[babelTransformRuntime, {
-            "regenerator": true
-          }
-        ]]
-      }})
+        inlineMap: true,
+        filename: path
+      })
+
+      const result = babelJest.createTransformer().process(js, path, options)
 
       return {
-        code: result.js,
-        map: result.v3SourceMap
+        code: result.code
       }
     }
     if (!/node_modules/.test(path)) {
